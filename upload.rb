493944650225bddc771e64718ecb25c47f449420
Kai Xiang <xiangkai@gmail.com>
2013-04-25 15:42:55 +0800

    copy directly from atmos rather than release directory

--- upload.rb
+++ upload.rb
@@ -3,21 +3,37 @@ require "yaml"
 require "multi_json"
 require "uri"
 require "base64"
+require "atmos"
+require "tempfile"
 
 ATOMS_PREFIX = 'rest/objects/'
 
+atmos_blob_option = {
+  :atmos_options => {
+    :url => 'http://blob.cfblob.com',
+    :uid => ENV['BLOB_ATMOS_UID'],
+    :secret => ENV['BLOB_ATMOS_SECRET']
+  },
+  :tag => ENV['BLOB_ATMOS_TAG']
+}
+
 s3_blob_option = {
   :aws_options => {
-    :access_key_id     => ENV['BOSH_AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['BOSH_AWS_SECRET_ACCESS_KEY'],
+    :access_key_id     => ENV['BLOB_AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['BLOB_AWS_SECRET_ACCESS_KEY'],
     :use_ssl           => true,
     :port              => 443
   },
   :bucket => ENV['BLOB_S3_BUCKET']
 }
 
-base_dir = File.join(ENV['BOSH_REPOSITORY'],"tmp", "cf-release")
-blobs_dir = File.join(base_dir, "blobs")
+def download_from_atmos(atmos_object, file_path)
+  File.open(file_path, "w") do |temp_file|
+    atmos_object.data_as_stream do |chunk|
+      temp_file.write(chunk)
+    end
+  end
+end
 
 def upload_to_s3(s3_object, file_path)
   File.open(file_path, "r") do |temp_file|
@@ -25,30 +41,24 @@ def upload_to_s3(s3_object, file_path)
   end
 end
 
-def decode_object_id(object_id)
-  begin
-    object_info = MultiJson.decode(Base64.decode64(URI::unescape(object_id)))
-  rescue MultiJson::DecodeError => e
-    raise 'Failed to parse object_id. Please try updating the release'
-  end
-
-  if !object_info.kind_of?(Hash) || object_info["oid"].nil? ||
-    object_info["sig"].nil?
-    raise "Invalid object_id (#{object_id})"
-  end
-  object_info
-end
-
-blob_yml = YAML.load_file(File.join(base_dir, "config", "blobs.yml"))
+Atmos::Parser::parser = Atmos::Parser::REXML
+store = Atmos::Store.new(atmos_blob_option[:atmos_options])
 aws_s3 = AWS::S3.new(s3_blob_option[:aws_options])
 
-blob_yml.each do |k, v|
-  blob_file = File.join(blobs_dir, k)
-  object_id = decode_object_id(v['object_id'])
-  oid = ATOMS_PREFIX + object_id["oid"]
-  puts "uploading #{blob_file} as #{oid}.."
-  s3_object = aws_s3.buckets[s3_blob_option[:bucket]].objects[oid]
-  upload_to_s3(s3_object, blob_file)
-  s3_object.acl = :public_read
+store.each_object_with_listable_tag(atmos_blob_option[:tag]) do |ob|
+  begin
+    tmp_file = Tempfile.open('download_object_file')
+    puts "Downloading #{ob.aoid} from Atmos..."
+    download_from_atmos(ob, tmp_file.path)
+    raise "Downloaded File #{ob.aoid} Broken" unless tmp_file.size.eql?ob.system_metadata['size'].to_i
+    oid = ATOMS_PREFIX + ob.aoid
+    s3_object = aws_s3.buckets[s3_blob_option[:bucket]].objects[oid]
+    puts "Uploading #{ob.aoid} to S3..."
+    upload_to_s3(s3_object, tmp_file)
+    s3_object.acl = :public_read
+  ensure
+    tmp_file.close
+    tmp_file.unlink
+  end
 end
 
